625a6bebf634b4f681e1bbb4c998a07e2412641c
tester001 <todut001@gmail.com>
2019-04-24 18:45:34 +0700

    version: 0.13 ~ 2019-04-24 18:45:26:475000

--- cmdw/__init__.py
+++ cmdw/__init__.py
@@ -1,5 +1,6 @@
 import imp
-version = imp.load_source('version', "../__version__.py")
+ROOT = os.path.dirname(os.path.dirname(__file__))
+version = imp.load_source('version', os.path.join(ROOT, "__version__.py"))
 __version__ 	= version.version
 __email__		= "licface@yahoo.com"
 __author__		= "licface@yahoo.com"
