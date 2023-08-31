e4e14389b6d996efe289f99cdc70b92652e0024c
hadi <hadi@dtp.net.id>
2023-08-31 16:12:53 +0700

    version: 0.21 ~ 2023-08-31 16:12:52:659389

--- cmdw/__init__.py
+++ cmdw/__init__.py
@@ -1,6 +1,14 @@
 import imp
-import __version__ as version
-__version__ 	= version.version
+
+error = False
+try:
+    import __version__ as version
+except:
+    from . import __version__ as version
+    error = True
+
+if not error:
+    __version__ 	= version.version
 __email__		= "licface@yahoo.com"
 __author__		= "licface@yahoo.com"
 
